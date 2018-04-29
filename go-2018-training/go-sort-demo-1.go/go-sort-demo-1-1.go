eea3c870649096b486f6f7cac03e98c316a1b79b
asm32cn <asm32@qq.com>
2018-04-29 13:52:11 +0800

    ...

---
+++ go-2018-training/go-sort-demo-1.go/go-sort-demo-1-1.go
@@ -0,0 +1,29 @@
+// go-sort-demo-1-1.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	items := []int{4, 202, 3, 9, 6, 5, 1, 43, 506, 2, 0, 8, 7, 100, 25, 4, 5, 97, 1000, 27}
+	bubbleSort( items )
+	fmt.Println( items )
+}
+
+func bubbleSort(items []int){
+	var (
+		n       = len(items)
+		swapped = true
+	)
+	for swapped {
+		swapped = false
+		for i := 0; i < n - 1; i++ {
+			if items[i] > items[i + 1] {
+				items[i + 1], items[i] = items[i], items[i + 1]
+				swapped = true
+			}
+		}
+		n = n - 1
+	}
+}
